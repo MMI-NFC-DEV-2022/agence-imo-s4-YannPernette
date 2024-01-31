cd75b53f40ce53cd317c358094e4f39b2d7c951c
YannPernette <yann.pernette@edu.univ-fcomte.fr>
2024-01-31 16:29:18 +0100

    ajout parametre par variable

--- src/pages/simple.vue
+++ src/pages/simple.vue
@@ -1,11 +1,26 @@
 <script setup lang="ts">
 import afficheMaison from '@/components/afficheMaison.vue';
+import { SchemaOffreMaison } from '@/types';
 
+const maMaison: SchemaOffreMaison = {
+    nomMaison: 'Maison',
+    prix: 1000,
+    nbrChambres: 3,
+    nbrSDB: 2,
+    adresse: '123 rue de la maison',
+    favori: true,
+    image: 'src\assets\img\pexels-binyamin-mellish-106399.jpg',
+    surface: "100",
+}
 </script>
 
 
 <template>
     <div class="mt-10">
-    <afficheMaison nomMaison = "Maison" favori image="src\assets\img\pexels-binyamin-mellish-106399.jpg"  />
-</div>
+        <afficheMaison nomMaison="Maison" favori image="src\assets\img\pexels-binyamin-mellish-106399.jpg" />
+    </div>
+
+    <div class="mt-10">
+        <afficheMaison :nomMaison="maMaison.nomMaison" />
+    </div>
 </template>
