a2013ecc4df6a50d9115dd588a6911ec7828c6f1
YannPernette <yann.pernette@edu.univ-fcomte.fr>
2024-01-31 15:39:09 +0100

    continue parametrique

--- src/pages/simple.vue
+++ src/pages/simple.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
 import afficheMaison from '@/components/afficheMaison.vue';
+
 </script>
 
 
 <template>
-<afficheMaison />
+    <div class="mt-10">
+    <afficheMaison nomMaison = "Maison" favori image="src\assets\img\pexels-binyamin-mellish-106399.jpg"  />
+</div>
 </template>
