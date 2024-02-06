ce4bba6d9cc9da27385e810a120a3ca11e1a5c1a
YannPernette <yann.pernette@edu.univ-fcomte.fr>
2024-02-06 15:32:36 +0100

    formulaire complet + styles

---
+++ src/components/formulaireOffreMaison.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { ref } from "@vue/reactivity";
+import afficheMaison from "./afficheMaison.vue";
+
+// On fait une variable reactive qui réference les données
+// ATTENTION : faire une Ref pas une Reactive car :
+// c'est l'objet qui doit être réactif, pas ses props
+
+const maison = ref();
+</script>
+
+
+<template>
+    <div>
+        <div class="p-2 mx-auto w-fit mb-20">
+            <h2 class="text-2xl mb-10 text-center"><span class="font-bold">Résultat</span> (Prévisualisation)</h2>
+            <!-- Optionnel on affiche les données -->
+            <afficheMaison v-bind="maison" />
+        </div>
+
+        <div class="p-2">
+            <!-- On passe la "ref" à FormKit -->
+            <FormKit type="form" v-model="maison" :config="{
+                classes: {
+                    form: 'border-2 border-gray-300 py-8 px-20 rounded-lg w-fit mx-auto',
+                    input: 'p-1 rounded border-gray-300 shadow-sm border mb-6 w-full',
+                    label: 'text-gray-600',
+                },
+            }" :submit-attrs="{ classes: { input: 'bg-red-300 p-4 mt-4 rounded w-full mb-0' } }">
+                <FormKit name="nomMaison" label="Nom de la maison" />
+                <FormKit name="prix" label="Prix de la maison" type="number" />
+                <FormKit name="favori" label="Mettre en valeur la maison" type="checkbox" wrapper-class="flex gap-2" />
+                <FormKit name="image" label="Image de la maison" />
+                <FormKit name="nbrChambres" label="Nombre de chambres de la maison" type="number" />
+                <FormKit name="nbrSDB" label="Nombre de salles de bain de la maison" type="number" />
+                <FormKit name="adresse" label="Adresse de la maison" />
+                <FormKit name="surface" label="Surface de la maison" />
+            </FormKit>
+        </div>
+    </div>
+</template>
--- src/pages/simple.vue
+++ src/pages/simple.vue
@@ -1,16 +1,21 @@
 <script setup lang="ts">
+import donnees from '../assets/data.json';
 import afficheMaison from '@/components/afficheMaison.vue';
-import { type  SchemaOffreMaison } from '@/types';
+import { type SchemaOffreMaison } from '@/types';
+
+const res = await fetch('../assets/data.json');
+
+const tableaumaisons:SchemaOffreMaison[] = donnees;
 
 const maMaison: SchemaOffreMaison = {
-    nomMaison: 'Maison',
+    nomMaison: 'Maison2',
     prix: 1000,
+    adresse: '123 rue',
     nbrChambres: 3,
     nbrSDB: 2,
-    adresse: '123 rue de la maison',
+    surface: "30x40m²",
     favori: true,
-    image: "src/assets/img/pexels-binyamin-mellish-106399.jpg",
-    surface: "100",
+    image: 'pexels-binyamin-mellish-106399 1 (5).png'
 }
 </script>
 
@@ -18,14 +23,23 @@ const maMaison: SchemaOffreMaison = {
 <template>
     <div class="mt-10">
         <h1>Etape 1 : </h1>
-        <afficheMaison nomMaison="Maison" :prix="200" :nbrChambres="32" :nbrSDB="67" adresse="oui" favori image="src/assets/img/pexels-binyamin-mellish-106399.jpg" surface="400" />
+        <afficheMaison nomMaison="Maison" :prix="200" :nbrChambres="32" :nbrSDB="67" adresse="oui" favori
+            image="src/assets/img/pexels-binyamin-mellish-106399.jpg" surface="400" />
     </div>
 
     <div class="mt-10">
         <afficheMaison v-bind="maMaison" />
     </div>
 
+    <div class="mt-10">
+        <afficheMaison v-bind="tableaumaisons[1]" />
+    </div>
+
     <!-- <div class="mt-10">
         <afficheMaison v-bind:nomMaison="maMaison.nomMaison" />
     </div> -->
+
+    <div class="mt-10">
+        <afficheMaison v-for="maison in tableaumaisons" :key="maison.nomMaison" />
+    </div>
 </template>
