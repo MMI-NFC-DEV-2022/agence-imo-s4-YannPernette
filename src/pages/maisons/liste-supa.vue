8ca3dc49ffc355ede259979e38f9b22160a984e4
YannPernette <yann.pernette@edu.univ-fcomte.fr>
2024-02-07 15:45:48 +0100

    liens parametriques (clic sur maison pour modifier si connecté)

--- src/pages/maisons/liste-supa.vue
+++ src/pages/maisons/liste-supa.vue
@@ -5,17 +5,21 @@ import afficheMaison from '@/components/afficheMaison.vue';
 import { supabase } from "@/supabase";
 console.log("supabase :", supabase); // pour vérifier et "garder" supabase dans le code
 const { data: Maison, error } = await supabase
-  .from('Maison')
-  .select('*')
+    .from('Maison')
+    .select('*')
 </script>
 
 
 <template>
-
     <h1 class="text-center text-3xl my-20 font-bold">Maisons appelées avec SupaBase</h1>
 
-    <div class="mt-10">
-        <afficheMaison v-for="maison in Maison" v-bind="maison" />
-    </div>
+    <div class="mt-10 mb-40 grid grid-cols-2 mx-20 justify-items-center">
 
+        <RouterLink v-for="maison in Maison" :key="maison.id"
+            :to="{ name: '/maisons/edit/[id]', params: { id: maison.id } }">
+                <afficheMaison v-bind="maison" class="mt-20" />
+        </RouterLink>
+
+
+    </div>
 </template>
