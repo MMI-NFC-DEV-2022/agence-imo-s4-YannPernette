824bae2268314833831db587e60fac3272476f5c
YannPernette <yann.pernette@edu.univ-fcomte.fr>
2024-02-07 10:03:37 +0100

    import types supabase + affichage des donnees

--- src/App.vue
+++ src/App.vue
@@ -10,8 +10,15 @@ import { RouterLink, RouterView } from 'vue-router'
       <RouterLink to="/">Home</RouterLink>
       <RouterLink to="/about">About</RouterLink>
       <RouterLink to="/simple">Simple</RouterLink>
+      <RouterLink to="/maisons/liste-supa">Liste Supa</RouterLink>
     </nav>
   </header>
 
+
+<Suspense>
   <RouterView />
+
+</Suspense>
+
+
 </template>
---
+++ src/pages/maisons/liste-supa.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import afficheMaison from '@/components/afficheMaison.vue';
+import { type SchemaOffreMaison } from '@/types';
+
+import { supabase } from "@/supabase";
+console.log("supabase :", supabase); // pour vérifier et "garder" supabase dans le code
+let { data: Maison, error } = await supabase
+  .from('Maison')
+  .select('*')
+</script>
+
+
+<template>
+
+    <h1 class="text-center text-3xl my-20 font-bold">Maisons appelées avec SupaBase</h1>
+
+    <div class="mt-10">
+        <afficheMaison v-for="maison in Maison" v-bind="maison" />
+    </div>
+
+</template>
