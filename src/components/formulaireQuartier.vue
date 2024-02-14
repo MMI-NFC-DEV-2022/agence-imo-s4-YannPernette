fde62be36f403f243a78a8ceea9e07a18d390804
YannPernette <yann.pernette@edu.univ-fcomte.fr>
2024-02-14 10:06:05 +0100

    rework menu + possibilité d'ajouter des quartiers dans leurs villes respectives

---
+++ src/components/formulaireQuartier.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { ref } from "@vue/reactivity";
+import { supabase } from "@/supabase";
+import { useRouter, useRoute } from "vue-router/auto";
+const router = useRouter();
+
+const quartier = ref();
+
+async function upsertQuartier(dataForm, node) {
+    const { data, error } = await supabase.from("Quartier").upsert(dataForm).select("id");
+    if (error) node.setErrors([error.message])
+    else {
+        console.log("data :", data);
+        node.setErrors([]);
+        router.push({ name: "/quartiers/edit/[[id]]", params: { id: data[0].id }, query: { success: true } });
+    }
+}
+
+const route = useRoute("/quartiers/edit/[[id]]");
+if (route.params.id) {
+    const { data, error } = await supabase.from("Quartier").select("*").eq("id", route.params.id).single();
+    if (error) console.error("error :", error);
+    else quartier.value = data;
+}
+</script>
+
+
+<template>
+    <div class="p-2">
+        <!-- On passe la "ref" à FormKit -->
+        <FormKit type="form" @submit="upsertQuartier" v-model="quartier" :config="{
+            classes: {
+                form: 'border-2 border-gray-300 py-8 px-20 rounded-lg w-fit mx-auto',
+                input: 'p-1 rounded border-gray-300 shadow-sm border mb-6 w-full',
+                label: 'text-gray-600',
+            },
+        }" :submit-attrs="{ classes: { input: 'bg-red-300 p-4 mt-4 rounded w-full mb-0' } }">
+            <FormKit name="nomQuartier" label="Nom du quartier" />
+            <FormKit name="id_Commune" label="Commune de référence" type="number" />
+        </FormKit>
+    </div>
+</template>
