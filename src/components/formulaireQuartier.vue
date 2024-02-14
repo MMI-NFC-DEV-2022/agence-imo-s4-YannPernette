33b28baf4567b465817c3ff62724c3c625375864
YannPernette <yann.pernette@edu.univ-fcomte.fr>
2024-02-14 11:24:32 +0100

    petite mise en forme formulaire

--- src/components/formulaireQuartier.vue
+++ src/components/formulaireQuartier.vue
@@ -12,7 +12,7 @@ async function upsertQuartier(dataForm, node) {
     else {
         console.log("data :", data);
         node.setErrors([]);
-        router.push({ name: "/quartiers/edit/[[id]]", params: { id: data[0].id }, query: { success: true } });
+        router.push("/quartiers");
     }
 }
 
@@ -63,13 +63,13 @@ async function supprimerQuartier() {
                 input: 'p-1 rounded border-gray-300 shadow-sm border mb-6 w-full',
                 label: 'text-gray-600',
             },
-        }" :submit-attrs="{ classes: { input: 'bg-red-300 p-4 mt-4 rounded w-full mb-0' } }">
+        }" :submit-attrs="{ classes: { input: 'bg-green-300 p-4 mt-4 rounded w-full mb-0' } }">
             <FormKit name="nomQuartier" label="Nom du quartier" />
             <!-- <FormKit name="id_Commune" label="Commune de référence" type="number" /> -->
             <FormKit type="select" name="id_Commune" label="Commune" :options="optionsCommune" />
 
             <button type="button" v-if="quartier.id" @click="($refs.dialogSupprimer as any).showModal()"
-            class="focus-style justify-self-end rounded-md bg-red-500 p-2 shadow-sm">
+            class="focus-style justify-self-end rounded-md bg-red-300 p-2 shadow-sm">
             Supprimer l'offre
         </button>
         <dialog ref="dialogSupprimer" @click="($event.currentTarget as any).close()">
